4f93774c6f130898dccdb7a2b63868165fd69385
jarochi <jaroslaw.chilimoniuk@gmail.com>
2018-10-22 17:41:52 +0200

    new

--- new_script_for_CV_results.R
+++ new_script_for_CV_results.R
@@ -4,32 +4,42 @@ library(reshape2)
 
 # mydata <- read_ods("Example_CV.ods", col_names = FALSE)
 mydata <- read_ods("measure1.ods", col_names = FALSE)
+plate_scheme <- read.csv("plate_scheme.csv")
 
-# check number of rows, check number of separate tables
-n_rows <- nrow(mydata)
-n_tables <- ceiling(n_rows/9)
-
-start_table <- seq(1, n_rows, 9)
+start_table <- seq(1, nrow(mydata), 9)
 end_table <- start_table + 8
+
 mediums <- c("LB10", "TSB", "BHI", "M63")
 
-# przerobić
 res <- lapply(split(mydata, ceiling(1L:nrow(mydata)/9)), function(ith_table) {
   raw_data <- as.matrix(ith_table[-1, ])
+
   class(raw_data) <- "numeric"
   colnames(raw_data) <- ith_table[1, ]
   rownames(raw_data) <- NULL
+  
+  conditions <- strsplit(ith_table[1,1], split = " ") %>% unlist()
+  # browser()
+  # 
+  # raw_data %>% matrix(nrow = 8)
+  # 
+  # raw_data <- raw_data %>%  mutate(temp = conditions[1],
+  #        surface = conditions[2])
 
+ 
+  
   not_NA_rows <- !apply(raw_data, 1, function(i) mean(is.na(i))) > 0.7
   not_NA_cols <- !apply(raw_data, 2, function(i) mean(is.na(i))) > 0.7
   if(sum(dim(raw_data[not_NA_rows, not_NA_cols])) == 0) {
     data.frame()
   } else {
-    cbind(melt(raw_data[not_NA_rows, not_NA_cols]), medium = unlist(lapply(mediums, rep, 3))) %>% 
+    cbind(melt(raw_data[not_NA_rows, not_NA_cols]), medium = unlist(lapply(mediums, rep, 3))) %>%
+      colnames(seq(1, nrow())) %>% 
+      inner_join(mplate_scheme, by = c("row" = "row", "col" = "col"))
       select(-Var1) %>%
       rename(strain = Var2)
+    browser()
   }
-
 }) %>%  bind_rows()
 
 
@@ -37,34 +47,28 @@ res <- lapply(split(mydata, ceiling(1L:nrow(mydata)/9)), function(ith_table) {
 # empty list to override bind_rows
 datalist = list()
 
-for (i in seq(1, n_tables)) {
+for (i in seq(1, ceiling(nrow(mydata)/9))) {
   # read start and end of single table
   x <- start_table[i]
   y <- end_table[i]
   raw_data <- mydata[x:y,]
-  # raw_data <- mydata[1:9,]     # TEST
-  # grab conditions
-  conditions <- raw_data[1,1]
   # transform single table
   strains_data <- raw_data[-c(2, 9), -c(1, 12)]
-  strains <- unique(as.integer(strains_data[1,]))
-  strains_results <- strains_data[-1,]
-  strains_results <- strains_results %>% unlist %>% matrix(nrow = 6)
-  
-  mresults <- melt(strains_results, varnames = c("row", "col")) %>% 
+  # strains_results <- strains_data[-1,] %>% unlist %>% matrix(nrow = 6)
+  mresults <- melt(strains_data[-1,] %>% unlist %>% matrix(nrow = 6), varnames = c("row", "col")) %>% 
     mutate(row = factor(row), 
            col = factor(col))
   
-  # create plate scheme for single table
-  strain_medium <- sapply(strains, function(x) (paste0(x, "-", mediums)))
-  strain_medium <- rep(strain_medium, each = 3)
-  plate_scheme <- matrix(strain_medium, nrow = 6)
-  # transform plate scheme
+  # create and transform plate scheme for single table
+  plate_scheme <- sapply(unique(as.integer(strains_data[1,])), function(x) (paste0(x, "-", mediums))) %>% 
+    rep(each = 3) %>% 
+    matrix(nrow = 6)
   mplate_scheme <- melt(plate_scheme, varnames = c("row", "col"), value.name = "description") %>% 
     mutate(row = factor(row), 
            col = factor(col))
   
   # create single df from single table
+  conditions <- raw_data[1,1] # grab conditions
   datalist[[i]] <- inner_join(mplate_scheme, mresults, by = c("row" = "row", "col" = "col")) %>% 
     filter(!is.na(description)) %>% 
     mutate(description = as.character(description), 
@@ -73,14 +77,11 @@ for (i in seq(1, n_tables)) {
            temp = sapply(strsplit(conditions, split = " "), first), 
            replicate = rep(1:3, times = nrow(mresults)/3),
            surface = sapply(strsplit(conditions, split = " "), last))  %>%
-    select(strain, medium, value, replicate, temp, surface) 
+    select(strain, medium, value, replicate, temp, surface) %>% 
+    filter(!is.na(value))
 } 
-# %>% 
-#   bind_rows()  # bind rows doesnt work
-
 
 all_results <- do.call(rbind, datalist)
-
 
 
 library(ggplot2)
@@ -89,13 +90,15 @@ library(ggbeeswarm)
 ggplot(all_results, aes(x = strain, y = value, color = temp, shape = surface)) +
   geom_boxplot() +
   facet_wrap(~ medium) +
-  theme_bw()
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 
 ggplot(all_results, aes(x = strain, y = value, color = temp, shape = surface)) +
   geom_quasirandom() +
   facet_wrap(~ medium) +
-  theme_bw()
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 group_by(all_results, strain, medium, temp, surface, replicate) %>%
   summarise(value = median(value)) %>%
@@ -103,7 +106,17 @@ group_by(all_results, strain, medium, temp, surface, replicate) %>%
   ggplot(aes(x = strain, y = value, color = temp, shape = surface)) +
   geom_point(size = 3) +
   facet_wrap(~ medium) +
-  theme_bw()
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+group_by(all_results, strain, medium, temp, surface) %>%
+  summarise(value = median(value)) %>%
+  summarise(value = median(value)) %>%
+  ggplot(aes(x = strain, y = value, color = temp, shape = surface)) +
+  geom_point(size = 3) +
+  facet_wrap(~ medium) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 # group_by(all_results, strain, medium, temp, surface, replicate) %>%
 #   mutate(value = value/max(value)) %>%
